3548168196bd7d49ac2731b86bc2d559d9db1a66
Boris Capitanu <capitanu@illinois.edu>
2012-11-29 13:55:43 +0000

    - added more detailed error reporting on failure

--- src-tools/org/seasr/meandre/components/tools/xml/io/ReadXML.java
+++ src-tools/org/seasr/meandre/components/tools/xml/io/ReadXML.java
@@ -45,6 +45,7 @@ package org.seasr.meandre.components.tools.xml.io;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
+import java.util.logging.Level;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -76,103 +77,110 @@ import org.xml.sax.SAXException;
  */
 
 @Component(
-		name = "Read XML",
-		creator = "Xavier Llora",
-		baseURL = "meandre://seasr.org/components/foundry/",
-		firingPolicy = FiringPolicy.all,
-		mode = Mode.compute,
-		rights = Licenses.UofINCSA,
-		tags = "#INPUT, semantic, io, read, xml",
-		description = "This component reads a XML. The XML location is specified " +
-				      "in the input. Also, it is able to read from URLs and local files " +
-				      "using URL of file syntax. The component outputs the semantic model " +
-				      "read. A property allows to control the behaviour of the component in " +
-				      "front of an IO error, allowing to continue pushing and empty XML or " +
-				      "throwing and exception forcing the finalization of the flow execution.",
-		dependency = {"protobuf-java-2.2.0.jar"}
+        name = "Read XML",
+        creator = "Xavier Llora",
+        baseURL = "meandre://seasr.org/components/foundry/",
+        firingPolicy = FiringPolicy.all,
+        mode = Mode.compute,
+        rights = Licenses.UofINCSA,
+        tags = "#INPUT, semantic, io, read, xml",
+        description = "This component reads a XML. The XML location is specified " +
+                      "in the input. Also, it is able to read from URLs and local files " +
+                      "using URL of file syntax. The component outputs the semantic model " +
+                      "read. A property allows to control the behaviour of the component in " +
+                      "front of an IO error, allowing to continue pushing and empty XML or " +
+                      "throwing and exception forcing the finalization of the flow execution.",
+        dependency = {"protobuf-java-2.2.0.jar"}
 )
 public class ReadXML extends AbstractExecutableComponent {
 
     //------------------------------ INPUTS ------------------------------------------------------
 
-	@ComponentInput(
-			name = Names.PORT_LOCATION,
-			description = "The URL or file name containing the model to read" +
+    @ComponentInput(
+            name = Names.PORT_LOCATION,
+            description = "The URL or file name containing the model to read" +
                 "<br>TYPE: java.net.URI" +
                 "<br>TYPE: java.net.URL" +
                 "<br>TYPE: java.lang.String" +
                 "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
-	)
-	protected static final String IN_LOCATION = Names.PORT_LOCATION;
+    )
+    protected static final String IN_LOCATION = Names.PORT_LOCATION;
 
     //------------------------------ OUTPUTS -----------------------------------------------------
 
-	@ComponentOutput(
-			name = Names.PORT_LOCATION,
-			description = "The URL or file name containing the model read" +
+    @ComponentOutput(
+            name = Names.PORT_LOCATION,
+            description = "The URL or file name containing the model read" +
                 "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
-	)
-	protected static final String OUT_LOCATION = Names.PORT_LOCATION;
+    )
+    protected static final String OUT_LOCATION = Names.PORT_LOCATION;
 
-	@ComponentOutput(
-			name = Names.PORT_XML,
-			description = "The XML object containing the document read" +
+    @ComponentOutput(
+            name = Names.PORT_XML,
+            description = "The XML object containing the document read" +
                 "<br>TYPE: org.w3c.dom.Document"
-	)
-	private final static String OUT_XML = Names.PORT_XML;
+    )
+    private final static String OUT_XML = Names.PORT_XML;
 
     //------------------------------ PROPERTIES --------------------------------------------------
 
-	// Inherited ignoreErrors (PROP_IGNORE_ERRORS) from AbstractExecutableComponent
+    // Inherited ignoreErrors (PROP_IGNORE_ERRORS) from AbstractExecutableComponent
 
-	@ComponentProperty(
+    @ComponentProperty(
             name = "validate_dtd",
             description = "Should validation be performed on DTDs?",
             defaultValue = "false"
     )
     protected static final String PROP_VALIDATE_DTD = "validate_dtd";
 
-	//--------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------
 
 
-	/** The document builder factory */
-	private DocumentBuilderFactory _factory;
+    /** The document builder factory */
+    private DocumentBuilderFactory _factory;
 
-	/** The document builder instance */
-	private DocumentBuilder _parser;
+    /** The document builder instance */
+    private DocumentBuilder _parser;
 
 
-	//--------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------
 
-	@Override
+    @Override
     public void initializeCallBack(ComponentContextProperties ccp) throws Exception {
-	    boolean validateDTDs = Boolean.parseBoolean(getPropertyOrDieTrying(PROP_VALIDATE_DTD, ccp));
+        boolean validateDTDs = Boolean.parseBoolean(getPropertyOrDieTrying(PROP_VALIDATE_DTD, ccp));
 
-	    _factory = DocumentBuilderFactory.newInstance();
-	    _factory.setNamespaceAware(true);
+        _factory = DocumentBuilderFactory.newInstance();
+        _factory.setNamespaceAware(true);
         _factory.setValidating(validateDTDs);
 
-	    _parser = _factory.newDocumentBuilder();
+        _parser = _factory.newDocumentBuilder();
 
-	    if (!validateDTDs) {
-    	    _parser.setEntityResolver(new EntityResolver() {
+        if (!validateDTDs) {
+            _parser.setEntityResolver(new EntityResolver() {
                 public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
                     return new InputSource(new StringReader(""));
                 }
             });
-	    }
-	}
+        }
+    }
 
-	@Override
+    @Override
     public void executeCallBack(ComponentContext cc) throws Exception {
-		URI location = DataTypeParser.parseAsURI(cc.getDataComponentFromInput(IN_LOCATION));
-		console.fine("Parsing location: " + location);
+        URI location = DataTypeParser.parseAsURI(cc.getDataComponentFromInput(IN_LOCATION));
+        console.fine("Parsing location: " + location);
 
-		Document doc = _parser.parse(StreamUtils.getInputStreamForResource(location));
+        Document doc;
+        try {
+            doc = _parser.parse(StreamUtils.getInputStreamForResource(location));
+        }
+        catch (Exception e) {
+            console.log(Level.SEVERE, "Could not parse document: " + location, e);
+            throw e;
+        }
 
-		cc.pushDataComponentToOutput(OUT_LOCATION, BasicDataTypesTools.stringToStrings(location.toString()));
-		cc.pushDataComponentToOutput(OUT_XML, doc);
-	}
+        cc.pushDataComponentToOutput(OUT_LOCATION, BasicDataTypesTools.stringToStrings(location.toString()));
+        cc.pushDataComponentToOutput(OUT_XML, doc);
+    }
 
     @Override
     public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
